17f4409c906d47b0867de0a3a0a46a72eba54c25
mitaksim1 <miriamt.simonnet@gmail.com>
2020-02-21 11:57:07 +0100

    button onClick dispatch action

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,14 +3,29 @@ import React from 'react';
 // connect suit le concept de high order component (partage d'information à un composant)
 import { connect } from 'react-redux';
 
-const Sidebar = ({ modules }) => (
+function toggleLesson(module, lesson) {
+    return {
+        type: 'TOGGLE_LESSON',
+        module,
+        lesson,
+    };
+}
+// quand on utilise la fonction connect() on a accès aussi à dispatch 
+// qui nous permettra de dispatcher des actions à redux
+// ces actions seront "entendus" par tous les reducers de l'application
+const Sidebar = ({ modules, dispatch }) => (
     <aside>
     {modules.map(module => (
         <div key={module.id}>
             <strong>{module.title}</strong>
             <ul>
                 {module.lessons.map(lesson => (
-                    <li key={lesson.id}>{lesson.title}</li>
+                    <li key={lesson.id}>
+                    {lesson.title}
+                    <button onClick={() => dispatch(toggleLesson(module, lesson))}>
+                    Selecionar
+                    </button>
+                    </li>
                 ))}
             </ul>
         </div>
